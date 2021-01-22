3381777c0aff136ed88c466fb7b322e2435ce275
Kyungeun <ruddms936@naver.com>
2021-01-22 23:43:51 +0900

    add: 게임 만들기 중간커밋

---
+++ 2/guess.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func main() {
+	seconds := time.Now().Unix()
+	rand.Seed(seconds)
+	target := rand.Intn(100) + 1
+	fmt.Println("I've chosen a randdom number between 1 and 100.")
+	fmt.Println("Can you guess it?")
+	fmt.Println(target)
+}
