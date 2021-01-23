155d3b59de1f10f7daea4b2c5e150865e622913f
Kyungeun <ruddms936@naver.com>
2021-01-24 01:26:28 +0900

    up: 함수화

--- 3/main.go
+++ 3/main.go
@@ -2,14 +2,13 @@ package main
 
 import "fmt"
 
+func paintNeeded(width float64, height float64) {
+	area := width * height
+	fmt.Printf("%.2f\n", area/10.0)
+}
+
 func main() {
-	var width, height, area float64
-	width = 4.2
-	height = 3.0
-	area = width * height
-	fmt.Printf("%.2f\n", area/10.0)
-	width = 5.2
-	height = 3.5
-	area = width * height
-	fmt.Printf("%.2f\n", area/10.0)
+	paintNeeded(4.2, 3.0)
+	paintNeeded(5.2, 3.5)
+	paintNeeded(5.0, 3.3)
 }
