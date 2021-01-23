77acd02a276cf661f478890771682efabb91a87c
Kyungeun <ruddms936@naver.com>
2021-01-24 00:00:50 +0900

    add: 3

---
+++ 3/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var width, height, area float64
+	width = 4.2
+	height = 3.0
+	area = width * height
+	fmt.Println(area/10.0, "liters neded")
+	width = 5.2
+	height = 3.5
+	area = width * height
+	fmt.Println(area/10.0, "liters neded")
+}
