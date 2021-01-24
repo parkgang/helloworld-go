7a0600cce48af708bc421a0837a1c056112f13e2
Kyungeun <ruddms936@naver.com>
2021-01-24 11:35:20 +0900

    up: error 처리

--- 3/main.go
+++ 3/main.go
@@ -1,14 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-func paintNeeded(width float64, height float64) float64 {
+func paintNeeded(width float64, height float64) (float64, error) {
+	if width < 0 {
+		return 0, fmt.Errorf("a width of %0.2f is invalid", width)
+	}
+	if height < 0 {
+		return 0, fmt.Errorf("a height of %0.2f is invalid", height)
+	}
 	area := width * height
-	return area / 10.0
+	return area / 10.0, nil
 }
 
 func main() {
-	fmt.Printf("%.2f\n", paintNeeded(4.2, 3.0))
-	fmt.Printf("%.2f\n", paintNeeded(5.2, 3.5))
-	fmt.Printf("%.2f\n", paintNeeded(5.0, 3.3))
+	amount, err := paintNeeded(4.2, -3.0)
+	fmt.Println(err)
+	fmt.Printf("%0.2f liters needed\n", amount)
 }
