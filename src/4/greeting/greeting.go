b16d11e97edb931b2af44f69b3f8aaa52e8f713d
Kyungeun <ruddms936@naver.com>
2021-01-24 18:55:01 +0900

    init

---
+++ src/4/greeting/greeting.go
@@ -0,0 +1,14 @@
+// Package greeting 패키지 연습
+package greeting
+
+import "fmt"
+
+// Hello 헬로우 출력
+func Hello() {
+	fmt.Println("Hello!")
+}
+
+// Hi 하이 출력
+func Hi() {
+	fmt.Println("Hi!")
+}
---
+++ src/4/hi/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"4/greeting"
+)
+
+func main() {
+	greeting.Hello()
+	greeting.Hi()
+}
