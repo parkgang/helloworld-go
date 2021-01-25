5e7dbf6a6d1c5405a41b4e6e73371fc9c828d88a
Kyungeun <ruddms936@naver.com>
2021-01-26 07:18:37 +0900

    add

---
+++ src/5/datafile/floats.go
@@ -0,0 +1,33 @@
+// Package datafile 파일로부터 샘플 데이터를 읽어 옵니다.
+package datafile
+
+import (
+	"bufio"
+	"os"
+	"strconv"
+)
+
+func GetFloats(fileName string) ([3]float64, error) {
+	var numbers [3]float64
+	file, err := os.Open(fileName)
+	if err != nil {
+		return numbers, err
+	}
+	i := 0
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		numbers[i], err = strconv.ParseFloat(scanner.Text(), 64)
+		if err != nil {
+			return numbers, err
+		}
+		i++
+	}
+	err = file.Close()
+	if err != nil {
+		return numbers, err
+	}
+	if scanner.Err() != nil {
+		return numbers, scanner.Err()
+	}
+	return numbers, nil
+}
