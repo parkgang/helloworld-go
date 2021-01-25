2ae9d153418c68962232ca4dffe6c15f3648b2b1
Kyungeun <ruddms936@naver.com>
2021-01-26 07:25:17 +0900

    add

--- src/5/average/main.go
+++ src/5/average/main.go
@@ -1,10 +1,17 @@
 // Package main 프로그램은 숫자들의 평균을 계산합니다.
 package main
 
-import "fmt"
+import (
+	"5/datafile"
+	"fmt"
+	"log"
+)
 
 func main() {
-	numbers := [3]float64{71.8, 56.2, 89.5}
+	numbers, err := datafile.GetFloats("../data.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
 	var sum float64
 	for _, number := range numbers {
 		sum += number
