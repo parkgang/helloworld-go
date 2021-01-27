0d085fa8c96d3011bdf77b52c1e386800df84597
Kyungeun <ruddms936@naver.com>
2021-01-27 22:17:16 +0900

    add

--- src/main.go
+++ src/main.go
@@ -1,18 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	items := map[string]string{
-		"이름": "박경은",
-		"나이": "22",
-	}
-	name, ok := items["이름"]
-	fmt.Println(name, ok)
-
-	age, ok := items["나이"]
-	fmt.Println(age, ok)
-
-	sex, ok := items["성"]
-	fmt.Println(sex, ok)
+	fmt.Println("플레이 그라운드")
 }
