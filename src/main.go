614cd57ddad88ef865a8df4362dc41112da2e254
박경은 <ruddms936@naver.com>
2021-01-27 13:06:42 +0900

    add

--- src/main.go
+++ src/main.go
@@ -3,7 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	var items map[string]int
+	items := map[string]string{
+		"이름": "박경은",
+		"나이": "22",
+	}
+	name, ok := items["이름"]
+	fmt.Println(name, ok)
 
-	fmt.Printf("%#v\n", items)
+	age, ok := items["나이"]
+	fmt.Println(age, ok)
+
+	sex, ok := items["성"]
+	fmt.Println(sex, ok)
 }
