d41c870ed3a4eaafe0fc4acba068a34562758921
Kyungeun <ruddms936@naver.com>
2021-01-30 01:25:42 +0900

    add

--- src/10/calendar/date.go
+++ src/10/calendar/date.go
@@ -3,29 +3,29 @@ package calendar
 import "errors"
 
 type Date struct {
-	Year  int
-	Month int
-	Day   int
+	year  int
+	month int
+	day   int
 }
 
 func (d *Date) SetYear(year int) error {
 	if year < 1 {
 		return errors.New("invalid year")
 	}
-	d.Year = year
+	d.year = year
 	return nil
 }
 func (d *Date) SetMonth(month int) error {
 	if month < 1 || month > 12 {
 		return errors.New("invalid month")
 	}
-	d.Month = month
+	d.month = month
 	return nil
 }
 func (d *Date) SetDay(day int) error {
 	if day < 1 || day > 31 {
 		return errors.New("invalid day")
 	}
-	d.Day = day
+	d.day = day
 	return nil
 }
--- src/10/main.go
+++ src/10/main.go
@@ -4,18 +4,22 @@ package main
 import (
 	"10/calendar"
 	"fmt"
+	"log"
 )
 
 func main() {
 	date := calendar.Date{}
-	date.Year = 2019
-	date.Month = 14
-	date.Day = 50
-	fmt.Println(date)
-	date = calendar.Date{
-		Year:  0,
-		Month: 0,
-		Day:   -2,
+	err := date.SetYear(2019)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = date.SetMonth(5)
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = date.SetDay(27)
+	if err != nil {
+		log.Fatal(err)
 	}
 	fmt.Println(date)
 }
