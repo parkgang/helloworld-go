333ea117efc11a94b8c3b9092b1b0e812bd34862
Kyungeun <ruddms936@naver.com>
2021-01-30 20:21:50 +0900

    add

--- src/11/main.go
+++ src/11/main.go
@@ -9,21 +9,16 @@ type Player interface {
 	Stop()
 }
 
-func playList(device Player, songs []string) {
-	for _, song := range songs {
-		device.Play(song)
+func TryOut(player Player) {
+	player.Play("Test Track")
+	player.Stop()
+	recorder, ok := player.(gadget.TapeRecorder)
+	if ok {
+		recorder.Record()
 	}
-	device.Stop()
 }
 
 func main() {
-	mixtape := []string{
-		"Jessie's Girl",
-		"Whip It",
-		"9 to 5",
-	}
-	var player Player = gadget.TapePlayer{}
-	playList(player, mixtape)
-	player = gadget.TapeRecorder{}
-	playList(player, mixtape)
+	TryOut(gadget.TapeRecorder{})
+	TryOut(gadget.TapePlayer{})
 }
