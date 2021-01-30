08b0a2e6d02d44c2f83474413c5905ee90891110
Kyungeun <ruddms936@naver.com>
2021-01-30 15:23:31 +0900

    add

---
+++ src/11/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"11/gadget"
+)
+
+func playList(device gadget.TapePlayer, songs []string) {
+	for _, song := range songs {
+		device.Play(song)
+	}
+	device.Stop()
+}
+
+func main() {
+	player := gadget.TapePlayer{}
+	mixtape := []string{
+		"Jessie's Girl",
+		"Whip It",
+		"9 to 5",
+	}
+	playList(player, mixtape)
+}
