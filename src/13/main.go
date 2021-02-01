de42988781d3fe2320e67a2079497cea3f44f30f
Kyungeun <ruddms936@naver.com>
2021-02-02 00:21:23 +0900

    add

--- src/13/main.go
+++ src/13/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("고루틴!")
+	response, err := http.Get("https://example.com")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer response.Body.Close()
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(string(body))
 }
