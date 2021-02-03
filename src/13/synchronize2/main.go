b4469e4eeace9a20c1de5e51a3db434176352768
Kyungeun <ruddms936@naver.com>
2021-02-03 22:31:30 +0900

    add

--- src/13/synchronize2/main.go
+++ src/13/synchronize2/main.go
@@ -7,24 +7,24 @@ import (
 
 func reportNap(name string, delay int) {
 	for i := 0; i < delay; i++ {
-		fmt.Println(name, "sleeping")
+		fmt.Println(name, "잔다")
 		time.Sleep(1 * time.Second)
 	}
-	fmt.Println(name, "wakes up!")
+	fmt.Println(name, "잠에서 깨다!")
 }
 
 func send(myChannel chan string) {
-	reportNap("sending goroutne", 2)
-	fmt.Println("***sending value***")
+	reportNap("고루틴 전송", 2)
+	fmt.Println("***전송 값***")
 	myChannel <- "a"
-	fmt.Println("***sending value***")
+	fmt.Println("***전송 값***")
 	myChannel <- "b"
 }
 
 func main() {
 	myChannel := make(chan string)
 	go send(myChannel)
-	reportNap("receiving goroutine", 5)
+	reportNap("고루틴 수신", 5)
 	fmt.Println(<-myChannel)
 	fmt.Println(<-myChannel)
 }
