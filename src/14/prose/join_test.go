4a05c373a1e2ce2780d69e3f50ff9bd1759030a6
Kyungeun <ruddms936@naver.com>
2021-02-07 00:20:08 +0900

    add

--- src/14/prose/join_test.go
+++ src/14/prose/join_test.go
@@ -3,9 +3,22 @@ package prose
 import "testing"
 
 func TestTwoElements(t *testing.T) {
-	t.Error("no test written yet")
+	list := []string{
+		"apple",
+		"orange",
+	}
+	if JoinWithCommas(list) != "apple and orange" {
+		t.Error("didn't match expected value")
+	}
 }
 
 func TestThreeElements(t *testing.T) {
-	t.Error("no test here either")
+	list := []string{
+		"apple",
+		"orange",
+		"pear",
+	}
+	if JoinWithCommas(list) != "apple, orange, and pear" {
+		t.Error("didn't match expected value")
+	}
 }
