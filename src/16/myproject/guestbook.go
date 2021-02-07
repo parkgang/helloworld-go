d947b7a9fb5ea47709bf2ef399322cfdc69ce964
Kyungeun <ruddms936@naver.com>
2021-02-07 22:07:59 +0900

    add

--- src/16/myproject/guestbook.go
+++ src/16/myproject/guestbook.go
@@ -9,17 +9,54 @@ import (
 	"text/template"
 )
 
+// Guestbook view.html을 렌더링할 때 사용하는 구조체입니다.
 type Guestbook struct {
 	SignatureCount int
 	Signatures     []string
 }
 
+// check nil이 아닌 에러가 전달되면 log.Fatal을 호출합니다.
 func check(err error) {
 	if err != nil {
 		log.Fatal(err)
 	}
 }
 
+// viewHandler 방명록 서명 목록을 읽어 전체 서명 개수와 함께 보여 줍니다.
+func viewHandler(writer http.ResponseWriter, request *http.Request) {
+	signatures := getStrings("signatures.txt")
+	html, err := template.ParseFiles("view.html")
+	check(err)
+	guestbook := Guestbook{
+		SignatureCount: len(signatures),
+		Signatures:     signatures,
+	}
+	err = html.Execute(writer, guestbook)
+	check(err)
+}
+
+// newHandler 서명을 입력할 폼을 보여 줍니다.
+func newHandler(writer http.ResponseWriter, request *http.Request) {
+	html, err := template.ParseFiles("new.html")
+	check(err)
+	err = html.Execute(writer, nil)
+	check(err)
+}
+
+// createHandler 추가할 서명 데이터가 담긴 POST 요청을 받아 서명 파일에 추가합니다.
+func createHandler(writer http.ResponseWriter, request *http.Request) {
+	signature := request.FormValue("signature")
+	options := os.O_WRONLY | os.O_APPEND | os.O_CREATE
+	file, err := os.OpenFile("signatures.txt", options, os.FileMode(0600))
+	check(err)
+	_, err = fmt.Fprintln(file, signature)
+	check(err)
+	err = file.Close()
+	check(err)
+	http.Redirect(writer, request, "/guestbook", http.StatusFound)
+}
+
+// getStrings 파일로부터 문자열을 한 줄씩 읽어 온 뒤 문자열 슬라이스를 반환합니다.
 func getStrings(fileName string) []string {
 	var lines []string
 	file, err := os.Open(fileName)
@@ -34,37 +71,6 @@ func getStrings(fileName string) []string {
 	}
 	check(scanner.Err())
 	return lines
-}
-
-func viewHandler(writer http.ResponseWriter, request *http.Request) {
-	signatures := getStrings("signatures.txt")
-	html, err := template.ParseFiles("view.html")
-	check(err)
-	guestbook := Guestbook{
-		SignatureCount: len(signatures),
-		Signatures:     signatures,
-	}
-	err = html.Execute(writer, guestbook)
-	check(err)
-}
-
-func newHandler(writer http.ResponseWriter, request *http.Request) {
-	html, err := template.ParseFiles("new.html")
-	check(err)
-	err = html.Execute(writer, nil)
-	check(err)
-}
-
-func createHandler(writer http.ResponseWriter, request *http.Request) {
-	signature := request.FormValue("signature")
-	options := os.O_WRONLY | os.O_APPEND | os.O_CREATE
-	file, err := os.OpenFile("signatures.txt", options, os.FileMode(0600))
-	check(err)
-	_, err = fmt.Fprintln(file, signature)
-	check(err)
-	err = file.Close()
-	check(err)
-	http.Redirect(writer, request, "/guestbook", http.StatusFound)
 }
 
 func main() {
