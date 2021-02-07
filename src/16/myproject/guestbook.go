c2e3e2fd4f92c0882e35539635706eeed9fe9b5b
Kyungeun <ruddms936@naver.com>
2021-02-07 21:37:23 +0900

    add

--- src/16/myproject/guestbook.go
+++ src/16/myproject/guestbook.go
@@ -47,8 +47,23 @@ func viewHandler(writer http.ResponseWriter, request *http.Request) {
 	check(err)
 }
 
+func newHandler(writer http.ResponseWriter, request *http.Request) {
+	html, err := template.ParseFiles("new.html")
+	check(err)
+	err = html.Execute(writer, nil)
+	check(err)
+}
+
+func createHandler(writer http.ResponseWriter, request *http.Request) {
+	signature := request.FormValue("signature")
+	_, err := writer.Write([]byte(signature))
+	check(err)
+}
+
 func main() {
 	http.HandleFunc("/guestbook", viewHandler)
+	http.HandleFunc("/guestbook/new", newHandler)
+	http.HandleFunc("/guestbook/create", createHandler)
 	err := http.ListenAndServe("localhost:8080", nil)
 	log.Fatal(err)
 }
