adbae8119e7243066e6d87cbe360ff06c79df6b2
Kyungeun <ruddms936@naver.com>
2021-02-07 17:27:19 +0900

    add

--- src/15/first-class-function/2/main.go
+++ src/15/first-class-function/2/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+func sayHi() {
+	fmt.Println("Hi")
+}
+func divide(a int, b int) float64 {
+	return float64(a) / float64(b)
+}
 
 func main() {
-	fmt.Println("")
+	var greeterFunction func()
+	var mathFunction func(int, int) float64
+	greeterFunction = sayHi
+	mathFunction = divide
+	greeterFunction()
+	fmt.Println(mathFunction(5, 2))
 }
